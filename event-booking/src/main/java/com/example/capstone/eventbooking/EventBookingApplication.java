594e186ac817ce1758bd7fac88920fbc498c0249
Vinay Reddy Janumpally <vinay.reddy.2318@gmail.com>
2023-10-06 01:32:38 -0400

    backend-project-created

---
+++ event-booking/src/main/java/com/example/capstone/eventbooking/EventBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone.eventbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingApplication.class, args);
+	}
+
+}
---
+++ event-booking/src/test/java/com/example/capstone/eventbooking/EventBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
