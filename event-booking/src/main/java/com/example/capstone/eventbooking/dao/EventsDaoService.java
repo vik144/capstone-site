6de94f87d2bf400db9d2dab5ba7d63c6ba2219a1
vinay <vinay.reddy.2318@gmail.com>
2023-11-27 15:27:46 -0500

    Backend Changes for events

--- event-booking/src/main/java/com/example/capstone/eventbooking/dao/EventsDaoService.java
+++ event-booking/src/main/java/com/example/capstone/eventbooking/dao/EventsDaoService.java
@@ -6,4 +6,17 @@ import com.example.capstone.eventbooking.entity.Event;
 
 public interface EventsDaoService {
 	public List<Event> retriveAllEvents();
+
+	public void addEvent(Event event);
+
+	public void updateEvent(Event event);
+
+	public void deleteEvent(int event_id);
+
+	public List<Event> getUserEvents(long user_id);
+
+	public Event getEvent(long event_id);
+
+	public void bookEvent(long user_id, long event_id);
+
 }
--- event-booking/src/main/java/com/example/capstone/eventbooking/service/EventBookingService.java
+++ event-booking/src/main/java/com/example/capstone/eventbooking/service/EventBookingService.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Service;
 
 import com.example.capstone.eventbooking.dao.EventsDaoService;
 import com.example.capstone.eventbooking.entity.Event;
+import com.example.capstone.eventbooking.entity.Users;
 import com.example.capstone.eventbooking.jpa.EventJpa;
+import com.example.capstone.eventbooking.jpa.UserJpa;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EventBookingService implements EventsDaoService{
@@ -16,17 +20,22 @@ public class EventBookingService implements EventsDaoService{
 	@Autowired
 	private EventJpa repository;
 	
+	@Autowired
+	private UserJpa userRepo;
+	
 	@Override
 	public List<Event> retriveAllEvents() {
 		// TODO Auto-generated method stub
 		return repository.findAll();
 	}
 
+	@Override
 	public void addEvent(Event event) {
 		// TODO Auto-generated method stub
 		repository.save(event);
 	}
 
+	@Override
 	public void updateEvent(Event event) {
 		// TODO Auto-generated method stub
 		Optional<Event> updateEvent = repository.findById((long)event.getEvent_id());
@@ -38,11 +47,42 @@ public class EventBookingService implements EventsDaoService{
 		repository.save(event);
 	}
 
+	@Override
 	public void deleteEvent(int event_id) {
 		// TODO Auto-generated method stub
 //		Optional<Event> event = repository.findById((long)event_id);
 		repository.deleteById((long)event_id);
 	}
-	
+
+	@Override
+	public Event getEvent(long event_id) {
+		// TODO Auto-generated method stub
+		Optional<Event> ent = repository.findById(event_id);
+		return ent.get();
+	}
+
+	@Override
+	public List<Event> getUserEvents(long user_id) {
+		Optional<Users> use = userRepo.findById(user_id);
+		return use.get().getEvents();
+	}
+
+	@Override
+	public void bookEvent(long user_id, long event_id) {
+		// TODO Auto-generated method stub
+		Optional<Event> optionalEvent = repository.findById(event_id);
+		Optional<Users> optionalUser = userRepo.findById(user_id);
+	    if (optionalEvent.isPresent() && optionalUser.isPresent()) {
+	        Event event = optionalEvent.get();
+	        Users user = optionalUser.get();
+
+	        // Set the user for the event
+	        event.setUser(user);
+	        repository.save(event);
+	    } else {
+	        // Handle the case where either the user or the event is not found
+	        throw new EntityNotFoundException("User or Event not found");
+	    }
+	}	
 	
 }
